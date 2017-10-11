34a66435d87ccb8d6fb3a158a1506eac7d6c83cc
chance <chancemagno@gmail.com>
2017-10-11 14:48:34 -0700

    add data binding/form validation to registration componenet

--- srs_mobile/app/registration/registration.component.ts
+++ srs_mobile/app/registration/registration.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as switchModule from "tns-core-modules/ui/switch";
-import { isValidPassword, isValidEmail } from "../utilities/utilities";
+import { isValidPassword, isValidEmail, isValidDisplayName, isValidFullName } from "../utilities/utilities";
 
 @Component({
   moduleId: module.id,
@@ -12,12 +12,15 @@ export class RegistrationComponent implements OnInit {
   private instructorStatus = false;
   private showForm = false;
   private identity : string = null;
-  private email: string = "testing@testing";
+  private email: string = "chance@gmail.com";
   private displayName: string;
   private fullName: string;
   private password: string;
   private confirmPassword: string;
-  private tempString: string = "Not valid right now";
+  private emailValidationString: string = "Not a valid email address";
+  private passwordValidationString: string = "Passwords don't match";
+  private displayNameValdiationString: string = "Display name is invalid";
+  private fullNameValidationString: string = "Full name is invalid";
 
   constructor() { }
 
@@ -28,13 +31,43 @@ export class RegistrationComponent implements OnInit {
     let mySwitch = new switchModule.Switch();  
   }
 
-  updateInputStatus(){
+  updateEmailInputStatus(){
     if(isValidEmail(this.email)){
-      this.tempString = "Valid email!";
-      return this.tempString;
+      this.emailValidationString = "Valid email!";
+      return this.emailValidationString;
     } else {
-      this.tempString = "still nope";
-      return this.tempString;
+      this.emailValidationString = "Not a valid email address";
+      return this.emailValidationString;
+    }
+  }
+
+  updatePasswordInputStatus(){
+    if(isValidPassword(this.password, this.confirmPassword)){
+      this.passwordValidationString = "Passwords match";
+      return this.passwordValidationString;
+    } else {
+      this.passwordValidationString = "Passwords don't match";
+      return this.passwordValidationString;
+    }
+  }
+
+  updateDisplayNameInputStatus(){
+    if(isValidDisplayName(this.displayName)){
+      this.displayNameValdiationString = "Valid display name";
+      return this.displayNameValdiationString;
+    } else{
+      this.displayNameValdiationString = "Display name is invalid";
+      return this.displayNameValdiationString;
+    }
+  }
+
+  updateFullNameInputStatus(){
+    if(isValidFullName(this.fullName)){
+      this.fullNameValidationString = "Valid full name";
+      return this.fullNameValidationString;
+    } else {
+      this.fullNameValidationString = "Invalid full name";
+      return this.fullNameValidationString;
     }
   }
 
@@ -42,6 +75,15 @@ export class RegistrationComponent implements OnInit {
     this.identity = identity;
     this.revealForm(true);
   }
+
+  isDirty(formInput: string){
+    if (formInput.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 
   createUser(){
     // if(isValidEmail(this.email)){
--- srs_mobile/app/utilities/utilities.ts
+++ srs_mobile/app/utilities/utilities.ts
@@ -10,4 +10,12 @@ export let isValidPassword = function (password: string, confirmPassword: string
   return password === confirmPassword && password != null && confirmPassword != null && password.length >7 && confirmPassword.length >7;
 }
 
+export let isValidDisplayName = function(displayName: string){
+  return displayName.length > 0; //TODO add more validation here
+}
+
+export let isValidFullName = function(fullName: string){
+  return fullName.length >0; //TODO flesh out as above
+}
+
 
